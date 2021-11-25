f0e0f28ccf57c1f383cbbc60e211b23df67a6125
Ivan-netizen <55799873+Ivan-netizen@users.noreply.github.com>
2021-11-25 10:26:16 -0600

    cambio

--- src/calculadoraactividad/Resta.java
+++ src/calculadoraactividad/Resta.java
@@ -7,7 +7,7 @@ package calculadoraactividad;
 public class Resta extends Operacion{
     
     double resta;
-       
+    double n=9;   
     public Resta(double n1, double n2) {
              
         super(n1, n2, '-');
